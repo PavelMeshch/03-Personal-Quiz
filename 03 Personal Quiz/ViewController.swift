818cd1b95f72559050210823e38a4a0dcf8d383d
macbook pro 665 <macbookpro665@macbook-mac133-2.local>
2019-03-14 20:32:58 +0300

    Initial Commit

---
+++ 03 Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  03 Personal Quiz
+//
+//  Created by macbook pro 665 on 14/03/2019.
+//  Copyright © 2019 Pavel Meshcheryakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
